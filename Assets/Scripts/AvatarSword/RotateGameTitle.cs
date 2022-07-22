7cae92d251f2c9ab76efc91e789b93001fda4c4c
Christian Sasso <csasso@usv.edu>
2022-07-22 12:51:48 -0700

    Added rotating title screen
    Added rotating title screen: initially, the Space Chicken's title is not visible, but it rotates for 3 seconds until it is.

---
+++ Assets/Scripts/AvatarSword/RotateGameTitle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class RotateGameTitle : MonoBehaviour
+{
+    [SerializeField] Transform titleTransform;
+    [SerializeField] float rotationDelay; // Time to rotate the title
+
+    private void Start()
+    {
+        StartCoroutine("Rotate");
+    }
+
+    IEnumerator Rotate()
+    {
+        const float xInitialAngle = 90;
+        const float xFinalAngle = 0;
+        float xCurrentAngle = 0;
+
+        float rotationAccumulator = 0;
+        while (rotationAccumulator < rotationDelay)
+        {
+            rotationAccumulator += Time.deltaTime;
+            xCurrentAngle = Mathf.LerpAngle(xInitialAngle, xFinalAngle, rotationAccumulator / rotationDelay);
+            titleTransform.rotation = Quaternion.Euler(xCurrentAngle, 0, 0);
+            yield return null;
+        }
+        titleTransform.rotation = Quaternion.Euler(0, 0, 0);
+        Debug.Log("Title completely rotated");
+    }
+}
